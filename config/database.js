02a26c8010d791df0c092465c4777b9d21a7ee5a
Ahmed Mouzoune <ahmedmouzoune@MacBook-Pro-de-Ahmed.local>
2022-03-21 10:23:38 +0100

    Config postgres

--- config/database.js
+++ config/database.js
@@ -1,11 +1,30 @@
-const path = require('path');
+// const path = require('path');
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
 
 module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', your_database_name),
+        username: env('DATABASE_USERNAME', your_username),
+        password: env('DATABASE_PASSWORD', your_password),
+        schema: 'public',
+      },
+      options: {},
     },
-    useNullAsDefault: true,
   },
 });
